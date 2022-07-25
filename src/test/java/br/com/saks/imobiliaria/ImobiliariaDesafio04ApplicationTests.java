dd6e427ad61a0b3df797ba0a527b04050c1d8b60
7839677 <7839677@U702617.univali.br>
2022-07-25 15:56:42 -0300

    CM - add

---
+++ src/test/java/br/com/saks/imobiliaria/ImobiliariaDesafio04ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saks.imobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariaDesafio04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
